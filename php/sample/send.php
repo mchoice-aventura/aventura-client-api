7aba9fcac2101cdc5b977f7113b0c4595c0dbd7e
arunoda <arunoda@hmspc051.hsenidmobile.com>
2010-10-06 10:55:31 +0530

    removed md5 password encryption added comments for smaples

--- php/sample/send.php
+++ php/sample/send.php
@@ -4,18 +4,29 @@
 	 * StandAlone SMS Sending
 	 */
 
+	//include the api for sending
 	include_once '../AppZoneSender.php';
 	
+	/*
+	creating the sender object with 
+		server url
+		appId
+		password
+	*/
 	$sender=new AppZoneSender(
-		"http://localhost:65182", 
+		"http://localhost:8080/appzone-simulator/simulator", 
 		"PD_ET_e0550", 
 		"098f6bcd4621d373cade4e832627b4f6");
 	
 	try{
+		//sending a one message
 		var_dump( $sender->sms("Message is this", array(78688,'0721683234')));
 		echo "<br>";
+		
+		//broadcasting a message
 		var_dump( $sender->smsToAll("Message is this"));
 	}
 	catch(AppZoneException $ex){
+		//throws when failed sending or receiving the sms
 		echo "ERROR::{$ex->getStatusMessage()}";
 	}
